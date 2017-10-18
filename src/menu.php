988a034b85946a2611702a1910048ee23dfd23e7
Sara <salvati.s@stesi.eu>
2017-10-18 12:12:30 +0200

    customer invoice and supplier invoice

--- src/menu.php
+++ src/menu.php
@@ -14,12 +14,24 @@ $menu_billing_invoice = [
                     "label" => Yii::t('app/menu/administration', 'Customer Invoice'),
                     "url" => ["/billing/invoice?invoice_type_id=1"]
                 ],
+                [
+                    "label" => Yii::t('app/menu/administration', 'Customer Preinvoice'),
+                    "url" => ["/billing/invoice?invoice_type_id=2"]
+                ],
+                [
+                    "label" => Yii::t('app/menu/administration', 'Customer Proforma'),
+                    "url" => ["/billing/invoice?invoice_type_id=3"]
+                ],
             ],
         ],
         [
             'label' => '<i class="fa fa-fw fa-asterisk"></i> ' . Yii::t('app/menu/administration', 'Purchase'),
             'url' => ["/billing/purchase"],
             'items' => [
+                [
+                    "label" => Yii::t('app/menu/administration', 'Supplier Prenvoice'),
+                    "url" => ["/billing/invoice?invoice_type_id=5"]
+                ],
                 [
                     "label" => Yii::t('app/menu/administration', 'Supplier Invoice'),
                     "url" => ["/billing/invoice?invoice_type_id=4"]
